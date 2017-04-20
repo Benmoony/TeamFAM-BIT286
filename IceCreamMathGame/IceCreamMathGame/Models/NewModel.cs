566a03b19226e0dc68db2c82f7d5648f40ac635f
Benmoony <benmiller92@gmail.com>
2017-04-20 11:28:03 -0700

    Added new model

---
+++ IceCreamMathGame/IceCreamMathGame/Models/NewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamMathGame.Models
+{
+    public class NewModel
+    {
+        //Start Building the Model class here
+    }
+}
