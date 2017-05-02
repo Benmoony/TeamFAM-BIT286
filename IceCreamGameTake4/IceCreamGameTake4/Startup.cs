24e88a5b646d770230c69a86fca96e3e16ded617
Benmoony <benmiller92@gmail.com>
2017-05-02 11:17:36 -0700

    Ignore these commits they will be overwritten

--- IceCreamGameTake4/IceCreamGameTake4/Startup.cs
+++ IceCreamGameTake4/IceCreamGameTake4/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using IceCreamGameTake4.Models;
 
 namespace IceCreamGameTake4
 {
@@ -29,6 +31,9 @@ namespace IceCreamGameTake4
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddDbContext<IceCreamGameTake4Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("IceCreamGameTake4Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
